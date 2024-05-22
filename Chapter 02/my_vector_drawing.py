dba9e744a854fa8fc0865f5e81efb97ea970276b
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2024-05-22 21:26:43 +0700

    first commit modify readme and chapter_02 add my_walkthrough notebook

---
+++ Chapter 02/my_vector_drawing.py
@@ -0,0 +1,25 @@
+from math import sqrt, pi, ceil, floor
+import matplotlib
+import matplotlib.patches
+from matplotlib.collections import PatchCollection
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import xlim, ylim
+
+blue = "C0"
+black = "k"
+red = "C3"
+green = "C2"
+purple = "C4"
+orange = "C2"
+gray = "gray"
+
+
+class Points:
+    def __init__(self, *vector, color=black):
+        self.vectors = list(vectors)
+        self.color = color
+
+
+# def draw(*objects, origin)
--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Source code for the book, Math for Programmers
 
 Up-to-date on GitHub at https://github.com/orlandpm/Math-for-Programmers
+
+## Local Start Up 
+
+**note**: already completed
+- automatic load python environment `math-for`
+# create a Jupyter Kernel for the environment
+- python -m ipykernel install --user --name=math-for --display-name="math-for"
+
+# start jupyter notebook (web browser)
+- jupyter notebook
